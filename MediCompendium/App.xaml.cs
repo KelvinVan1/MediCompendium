6c6647475752a4a986029ad9fe7ee8a97e2d6f98
KelvinVan1 <90666201+KelvinVan1@users.noreply.github.com>
2024-06-20 20:46:02 -0700

    Initial Commit

---
+++ MediCompendium/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MediCompendium;
+
+public partial class App : Application {
+    public App() {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MediCompendium/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace MediCompendium;
+
+public partial class AppShell : Shell {
+    public AppShell() {
+        InitializeComponent();
+    }
+}
