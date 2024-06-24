737ab046a633769470014587af32099c41b3484a
Kelvin Van <kelvinvan.cs@gmail.com>
2024-06-24 16:11:39 -0700

    Catching errors when fetching and function documentation

--- MediCompendium/Services/ApiCommands.cs
+++ MediCompendium/Services/ApiCommands.cs
@@ -1,16 +1,29 @@
+using System.Collections.Specialized;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
+using System.Web;
 using MediCompendium.Models.ApiRecords;
 
 namespace MediCompendium.Services;
 
 public class ApiCommands {
-    
-    public static async void FetchMedications()  {
-        HttpResponseMessage responseMessage = await ApiClient.Client.GetAsync(Constants.NdcRoute);
-        var response = await responseMessage.Content.ReadFromJsonAsync<JsonDocument>();
-        var content = response.RootElement.GetProperty("results").GetRawText();
-        var data = JsonSerializer.Deserialize<List<NdcData>>(content);
+    /// <summary>
+    /// Fetches a list of medications from the OpenFDA API
+    /// </summary>
+    /// <returns>A list of medication NDC data. Returns an empty list if request fails</returns>
+    public static async Task<List<NdcData>> FetchMedications() {
+        try {
+            HttpResponseMessage responseMessage = await ApiClient.Client.GetAsync($"{Constants.NdcRoute}{Constants.Limit}");
+            var response = await responseMessage.Content.ReadFromJsonAsync<JsonDocument>();
+            var content = response.RootElement.GetProperty("results").GetRawText();
+            return JsonSerializer.Deserialize<List<NdcData>>(content) ?? new List<NdcData>();
+        }
+        catch(Exception err) {
+            Console.WriteLine(err);
+            return new List<NdcData>();
+        }
     }
+    
+    
 }
--- MediCompendium/Services/Constants.cs
+++ MediCompendium/Services/Constants.cs
@@ -1,5 +1,7 @@
 namespace MediCompendium.Services;
 
-static class Constants {
-    public const string NdcRoute = "https://api.fda.gov/drug/ndc.json";
+static class Constants
+{
+    public const string NdcRoute = "https://api.fda.gov/drug/ndc.json?";
+    public const string Limit = "limit=5";
 }
