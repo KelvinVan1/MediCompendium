6aeee240e58b8009ad33d8f860839e0e4acc9e99
Kelvin Van <kelvinvan.cs@gmail.com>
2024-06-24 22:47:50 -0700

    Structure for MedicationList page completed

--- MediCompendium/MainPage.xaml.cs
+++ MediCompendium/MainPage.xaml.cs
@@ -3,21 +3,11 @@
 namespace MediCompendium;
 
 public partial class MainPage : ContentPage {
-    int count = 0;
-
     public MainPage() {
         InitializeComponent();
     }
 
-    private void OnContinueClicked(object sender, EventArgs e) {
-        ApiCommands.FetchMedications();
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
+    private async void OnContinueClicked(object sender, EventArgs e) {
+        await Shell.Current.GoToAsync("//MedicationList");
     }
 }
---
+++ MediCompendium/Views/MedicationItem.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediCompendium.Views;
+
+public partial class MedicationItem : ContentView {
+    public MedicationItem() {
+        InitializeComponent();
+    }
+}
