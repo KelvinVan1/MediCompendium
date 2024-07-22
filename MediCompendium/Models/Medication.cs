cd0a61aca2f4a1351158a3584cf772b534b42f52
Kelvin Van <kelvinvan.cs@gmail.com>
2024-07-22 15:22:57 -0700

    Populate active ingredients Starting code for tap to view medication functionality

--- MediCompendium/Models/Medication.cs
+++ MediCompendium/Models/Medication.cs
@@ -1,18 +1,32 @@
 using MediCompendium.Models.ApiRecords;
-
 namespace MediCompendium.Models;
 
 public class Medication {
     public string? BrandName { get; set; }
     public string? GenericName { get; set; }
     public string? LabelerName { get; set; }
-    public List<ActiveIngredients> ActiveIngredients { get; set; }
+    public List<ActiveIngredients>? ActiveIngredients { get; set; }
     
     public Medication(){}
     
-    public Medication(string brandName, string genericName, string labelerName) {
+    public Medication(string brandName, string genericName, string labelerName, List<ActiveIngredients> activeIngredients) {
         BrandName = brandName;
         GenericName = genericName;
         LabelerName = labelerName;
+        ActiveIngredients = activeIngredients;
+    }
+
+    public string ActiveIngredientsToString(int count) {
+        var result = "";
+        
+        if (ActiveIngredients == null || ActiveIngredients.Count == 0) return "Not Provided";
+        if (ActiveIngredients.Count < count) count = ActiveIngredients.Count;
+        
+        for (var i = 0; i < count; i++) {
+            var ingredient = ActiveIngredients[i].name.ToLower() + " " + ActiveIngredients[i].strength + ", ";
+            result += ingredient;
+        }
+
+        return result.Remove(result.Length-2, 2);
     }
 }
--- MediCompendium/Views/MedicationItem.xaml.cs
+++ MediCompendium/Views/MedicationItem.xaml.cs
@@ -4,30 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MediCompendium.Models;
+using MediCompendium.Pages;
 
 namespace MediCompendium.Views;
 
 public partial class MedicationItem : ContentView {
+    public static readonly BindableProperty CurrentMedicationProperty = BindableProperty.Create(
+        nameof(CurrentMedication),
+        typeof(Medication),
+        typeof(MedicationItem));
 
-    public static readonly BindableProperty MedicationProperty =
-        BindableProperty.Create(nameof(Medication), typeof(Medication), typeof(MedicationItem));
-
-    public Medication Medication {
-        get { return (Medication)GetValue(MedicationProperty); }
-        set { SetValue(MedicationProperty, value); }
+    public Medication CurrentMedication {
+        get => (Medication)GetValue(CurrentMedicationProperty);
+        set => SetValue(CurrentMedicationProperty, value);
     }
-    
+
     public MedicationItem() {
         InitializeComponent();
+        BindingContextChanged += OnBindingContextChanged;
     }
-
+    
+    private void OnBindingContextChanged(object sender, EventArgs e) {
+        if (BindingContext is Medication medication)
+            ActiveIngredientLabel.Text = medication.ActiveIngredientsToString(3);
+    }
+    
     private void OnFavoriteTapped(object sender, EventArgs e) {
         var currentImage = FavoriteHeart.Source.ToString();
         FavoriteHeart.Source = currentImage.Contains("heart.png") ? "heart_filled.png" : "heart.png";
     }
-    protected override void OnPropertyChanged(string propName = null) {
-        base.OnPropertyChanged(propName);
-        if (propName == MedicationProperty.PropertyName)
-            BindingContext = Medication;
+
+    private void OnMedicationTapped(object sender, EventArgs e) {
+        Console.WriteLine("Medication Tapped!");
     }
+    
 }
