eea770c067797fa188c5a63471e0a2374ddb236a
KelvinVan1 <90666201+KelvinVan1@users.noreply.github.com>
2024-08-04 14:36:28 -0700

    ProfileSelection page population

--- MediCompendium/Pages/ProfileSelection.xaml.cs
+++ MediCompendium/Pages/ProfileSelection.xaml.cs
@@ -6,16 +6,34 @@ using System.Text;
 using System.Threading.Tasks;
 using MediCompendium.Models;
 using MediCompendium.Models.DbTables;
+using MediCompendium.Services;
 
 namespace MediCompendium.Pages;
 
 public partial class ProfileSelection : ContentPage {
     public ObservableCollection<UserProfile> UserProfiles { get; set; }
+    private DbCommands? _db;
     
     public ProfileSelection() {
         InitializeComponent();
+        UserProfiles = new ObservableCollection<UserProfile>();
+        _db = new DbCommands();
+        BindingContext = this;
     }
 
+    protected override void OnAppearing() {
+        base.OnAppearing();
+        GenerateProfiles();
+    }
+
+    private async void GenerateProfiles() {
+        var profiles = await _db.GetProfiles();
+        UserProfiles.Clear();
+        foreach (var profile in profiles) {
+            UserProfiles.Add(profile);
+        }
+    }
+    
     private void OnCreateProfileClicked(object sender, EventArgs e) {
         Shell.Current.GoToAsync("//ProfileCreation");
     }
