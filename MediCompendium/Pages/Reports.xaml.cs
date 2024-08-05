6624fe7d2f9c9dc63e9a99ba255bd8c8ba1cb2f4
KelvinVan1 <90666201+KelvinVan1@users.noreply.github.com>
2024-08-05 16:48:15 -0700

    Changes to report page   now has a row/column style

--- MediCompendium/Pages/Reports.xaml.cs
+++ MediCompendium/Pages/Reports.xaml.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using MediCompendium.Models;
 using MediCompendium.Models.DbTables;
 using MediCompendium.Services;
-using MediCompendium.Views;
 
 namespace MediCompendium.Pages;
 
@@ -42,43 +41,64 @@ public partial class Reports : ContentPage {
 
     private void DisplaySavedMedications() {
         GenerateReportHeading("Saved Medication List");
+        var grid = new Grid() {ColumnSpacing = 5};
         
-        foreach (var medication in MedicationList) {
-            var reportItem = new MedicationItemReport() {
-                CurrentMedication = medication
-            };
-            ReportContent.Children.Add(reportItem);
+        CreateColumns(grid, 3);
+        CreateMedicationColumns(grid);
+        
+        CreateRows(grid, MedicationList.Count);
+
+        for (var i = 0; i < MedicationList.Count; i++) {
+            grid.Add(new Label() {Text = $"{MedicationList[i].BrandName}"}, 0, i + 1);
+            grid.Add(new Label() {Text = $"{MedicationList[i].LabelerName}"}, 1, i + 1);
+            grid.Add(new Label() {Text = $"{MedicationList[i].ActiveIngredientsToString()}"}, 2, i + 1);
         }
+        
+        ReportContent.Add(grid);
     }
 
     private void DisplayOtcMedications() {
         GenerateReportHeading("Saved Over the Counter Medication List");
+        var grid = new Grid() {ColumnSpacing = 5};
         
-        foreach (var medication in MedicationList) {
-            if (medication.ProductType == "HUMAN OTC DRUG") {
-                var reportItem = new MedicationItemReport() {
-                    CurrentMedication = medication
-                };
-                ReportContent.Children.Add(reportItem);
+        CreateColumns(grid, 3);
+        CreateMedicationColumns(grid);
+        
+        CreateRows(grid, MedicationList.Count);
+
+        for (var i = 0; i < MedicationList.Count; i++) {
+            if (MedicationList[i].ProductType == "HUMAN OTC DRUG") {
+                grid.Add(new Label() {Text = $"{MedicationList[i].BrandName}"}, 0, i + 1);
+                grid.Add(new Label() {Text = $"{MedicationList[i].LabelerName}"}, 1, i + 1);
+                grid.Add(new Label() {Text = $"{MedicationList[i].ActiveIngredientsToString()}"}, 2, i + 1);
             };
         }
+        
+        ReportContent.Add(grid);
     }
 
     private void DisplayPrescriptionMedications() {
         GenerateReportHeading("Saved Prescription Medication List");
+        var grid = new Grid() {ColumnSpacing = 5};
         
-        foreach (var medication in MedicationList) {
-            if (medication.ProductType == "HUMAN PRESCRIPTION DRUG") {
-                var reportItem = new MedicationItemReport() {
-                    CurrentMedication = medication
-                };
-                ReportContent.Children.Add(reportItem);
+        CreateColumns(grid, 3);
+        CreateMedicationColumns(grid);
+        
+        CreateRows(grid, MedicationList.Count);
+
+        for (var i = 0; i < MedicationList.Count; i++) {
+            if (MedicationList[i].ProductType == "HUMAN PRESCRIPTION DRUG") {
+                grid.Add(new Label() {Text = $"{MedicationList[i].BrandName}"}, 0, i + 1);
+                grid.Add(new Label() {Text = $"{MedicationList[i].LabelerName}"}, 1, i + 1);
+                grid.Add(new Label() {Text = $"{MedicationList[i].ActiveIngredientsToString()}"}, 2, i + 1);
             };
         }
+        
+        ReportContent.Add(grid);
     }
 
     private void DisplayMedicationCount() {
-        GenerateReportHeading("Saved Medication counts (prescription and OTC): ");
+        GenerateReportHeading("Saved Medication Counts (Prescription and OTC): ");
         
         var otcCount = 0;
         var prescriptionCount = 0;
@@ -88,15 +108,34 @@ public partial class Reports : ContentPage {
             else otcCount++;
         }
 
-        var otcLabel = new Label();
-        otcLabel.Text = $"Saved Over the Counter Medications: {otcCount}";
-        var prescriptionLabel = new Label();
-        prescriptionLabel.Text = $"Saved Prescription Medications: {prescriptionCount}";
+        var grid = new Grid() {ColumnSpacing = 5};
         
-        ReportContent.Children.Add(otcLabel);
-        ReportContent.Children.Add(prescriptionLabel);
+        CreateColumns(grid, 2);
+        grid.Add(new Label() {Text = "Over the Counter"}, 0, 0);
+        grid.Add(new Label() {Text = "Prescription"}, 1, 0);
+        
+        CreateRows(grid, 1);
+        grid.Add(new Label() {Text = $"{otcCount}"}, 0, 1);
+        grid.Add(new Label() {Text = $"{prescriptionCount}"}, 1, 1);
+        ReportContent.Add(grid);
     }
 
+    private void CreateColumns(Grid grid, int count) {
+        for(var i = 0; i < count; i++) 
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+    }
+    
+    private void CreateMedicationColumns(Grid grid) {
+        grid.Add(new Label() {Text = "Name", Style=(Style)Application.Current.Resources["ReportHeader"]}, 0, 0);
+        grid.Add(new Label() {Text = "Labeler", Style=(Style)Application.Current.Resources["ReportHeader"]}, 1, 0);
+        grid.Add(new Label() {Text = "Active Ingredient", Style=(Style)Application.Current.Resources["ReportHeader"]}, 2, 0);
+    }
+    
+    private void CreateRows(Grid grid, int count) {
+        for(var i = 0; i < count; i++) 
+            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+    }
+    
     private void GenerateReportHeading(string message) {
         ReportContent.Children.Add(new Label() {
             Text = message, 
--- MediCompendium/Services/DbCommands.cs
+++ MediCompendium/Services/DbCommands.cs
@@ -9,82 +9,132 @@ public class DbCommands {
     public DbCommands() { }
 
     // Profile creation and management
-    private async Task InitUserProfile()
-    {
-        if (_database is not null) return;
+    private async Task InitUserProfile() {
+        try {
+            if (_database is not null) return;
 
-        _database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
-        await _database.CreateTableAsync<UserProfile>();
+            _database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            await _database.CreateTableAsync<UserProfile>();
+        }
+        catch (Exception err) {
+            Console.WriteLine($"An error has occured. {err}");
+        }
     }
 
-    private async Task InitFavoriteMedication()
-    {
-        if (_database is not null) return;
+    private async Task InitFavoriteMedication() {
+        try {
+            if (_database is not null) return;
 
-        _database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
-        await _database.CreateTableAsync<FavoritedItem>();
+            _database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            await _database.CreateTableAsync<FavoritedItem>();
+        }
+        catch (Exception err) {
+            Console.WriteLine($"An error has occured. {err}");
+        }
     }
     
     public async Task<int> AddProfile(UserProfile profile) {
-       await InitUserProfile();
+        try {
+            await InitUserProfile();
        
-       if (_database == null) return 0;
+            if (_database == null) return 0;
        
-       if (profile.Id == 0) 
-           return await _database.InsertAsync(profile);
-       else
-           return await _database.UpdateAsync(profile);
+            if (profile.Id == 0) 
+                return await _database.InsertAsync(profile);
+            else
+                return await _database.UpdateAsync(profile);
+        }
+        catch (Exception err) {
+            Console.WriteLine($"An error has occured. {err}");
+            return 0;
+        }
     }
 
     public async Task<int> DeleteProfile(int id) {
-        await InitUserProfile();
+        try {
+            await InitUserProfile();
 
-        if (_database == null) return 0;
+            if (_database == null) return 0;
 
-        var query = "DELETE FROM profile WHERE id = ?";
-        return await _database.ExecuteAsync(query, id);
+            var query = "DELETE FROM profile WHERE id = ?";
+            return await _database.ExecuteAsync(query, id);
+        }
+        catch (Exception err) {
+            Console.WriteLine($"An error has occured. {err}");
+            return 0;
+        }
     }
     
     public async Task<List<UserProfile>> GetProfiles() {
-        await InitUserProfile();
+        try {
+            await InitUserProfile();
 
-        if (_database == null) return new List<UserProfile>();
+            if (_database == null) return new List<UserProfile>();
 
-        return await _database.Table<UserProfile>().ToListAsync();
+            return await _database.Table<UserProfile>().ToListAsync();
+        }
+        catch (Exception err) {
+            Console.WriteLine($"An error has occured. {err}");
+            return new List<UserProfile>();
+        }
     }
     
     public async Task<int> AddFavorite(FavoritedItem item) {
-        await InitFavoriteMedication();
+        try {
+            await InitFavoriteMedication();
 
-        if (_database == null) return 0;
+            if (_database == null) return 0;
 
-        return await _database.InsertAsync(item);
+            return await _database.InsertAsync(item);
+        }
+        catch (Exception err) {
+            Console.WriteLine($"An error has occured. {err}");
+            return 0;
+        }
     }
     
     public async Task<int> RemoveFavorite(FavoritedItem item) {
-        await InitFavoriteMedication();
+        try {
+            await InitFavoriteMedication();
 
-        if (_database == null) return 0;
+            if (_database == null) return 0;
 
-        const string query = "DELETE FROM favorite_items WHERE (product_ndc = ? AND profile_id = ?);";
-        return await _database.ExecuteAsync(query, item.ProductNdc, item.ProfileId);
+            const string query = "DELETE FROM favorite_items WHERE (product_ndc = ? AND profile_id = ?);";
+            return await _database.ExecuteAsync(query, item.ProductNdc, item.ProfileId);
+        }
+        catch (Exception err) {
+            Console.WriteLine($"An error has occured. {err}");
+            return 0;
+        }
     }
 
     public async Task<List<FavoritedItem>> SearchFavoriteItem(int userId, string productNdc) {
-        await InitFavoriteMedication();
+        try {
+            await InitFavoriteMedication();
 
-        if (_database == null) return new List<FavoritedItem>();
+            if (_database == null) return new List<FavoritedItem>();
 
-        const string query = "SELECT * FROM favorite_items WHERE (product_ndc = ? AND profile_id = ?)"; 
-        return await _database.QueryAsync<FavoritedItem>(query, productNdc, userId);
+            const string query = "SELECT * FROM favorite_items WHERE (product_ndc = ? AND profile_id = ?)";
+            return await _database.QueryAsync<FavoritedItem>(query, productNdc, userId);
+        }
+        catch (Exception err) {
+            Console.WriteLine($"An error has occured. {err}");
+            return new List<FavoritedItem>();
+        }
     }
     
     public async Task<List<FavoritedItem>> GetFavoriteItems(int userId) {
-        await InitFavoriteMedication();
+        try {
+            await InitFavoriteMedication();
 
-        if (_database == null) return new List<FavoritedItem>();
+            if (_database == null) return new List<FavoritedItem>();
 
-        const string query = "SELECT * FROM favorite_items WHERE profile_id = ?";
-        return await _database.QueryAsync<FavoritedItem>(query, userId);
+            const string query = "SELECT * FROM favorite_items WHERE profile_id = ?";
+            return await _database.QueryAsync<FavoritedItem>(query, userId);
+        }
+        catch (Exception err) {
+            Console.WriteLine($"An error has occured. {err}");
+            return new List<FavoritedItem>();
+        }
     }
 }
